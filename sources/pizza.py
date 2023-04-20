4b8a6b41a55b8acb03553c704a7fc35d2cffda1b
vince callarec <callarecvincent@yahoo.fr>
2023-04-20 14:58:55 +0200

    fix error with no sauce pizzas in pizza.py

--- sources/pizza.py
+++ sources/pizza.py
@@ -3,65 +3,51 @@ RECIPES = {
     'marinara': {
         'ingredients': {'garlic': 2, 'basil': 3},
         'price': 9,
+        'sauce': 'tomato',
     },
     'romana': {
         'ingredients': {'ham': 4, 'olive': 5, 'onion': 2, 'basil': 2},
         'price': 12,
+        'sauce': 'tomato',
     },
     'original': {
         'ingredients': {'garlic': 5, 'pepper': 4, 'parmesan': 2},
         'price': 10,
-        'sauce': 'cream'
+        'sauce': 'cream',
     },
     'ultima': {
         'ingredients': {'gorgonzola': 4, 'pepper': 5, 'onion': 4, 'ham': 3},
         'price': 12,
-        'sauce': 'cream'
+        'sauce': 'cream',
     },
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+        'sauce': 'cream',
     },
     'margherita': {
         'ingredients': {'mozzarella': 2, 'oregano': 4},
         'price': 10,
-        'sauce': 'tomato'
+        'sauce': 'tomato',
     },
     'quattro fromagi': {
         'ingredients': {'mozzarella': 2, 'parmesan': 2, 'gorgonzola': 2, 'cheddar': 2, 'oregano': 3},
         'price': 12,
-        'sauce': 'tomato'
+        'sauce': 'tomato',
     },
     'crudo': {
         'ingredients': {'mozzarella': 2, 'ham': 3, 'basil': 3, 'mushroom': 3},
         'price': 11,
-        'sauce': 'tomato'
+        'sauce': 'tomato',
     },
     'pugliese': {
         'ingredients': {'mozzarella': 4, 'oregano': 2, 'onion': 5, 'mushroom': 2},
         'price': 11,
-        'sauce': 'tomato'
-    },
-    'romana': {
-        'ingredients': {'ham': 4, 'olive': 3, 'onion': 2, 'basil': 2},
-        'price': 12,
-    },
-    'original': {
-        'ingredients': {'garlic': 5, 'pepper': 4, 'parmesan': 2},
-        'price': 10,
-        'sauce': 'cream'
-    },
-    'ultima': {
-        'ingredients': {'gorgonzola': 4, 'pepper': 5, 'onion': 4, 'ham': 3},
-        'price': 12,
-        'sauce': 'cream'
-    },
-    'mediterranea': {
-        'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
-        'price': 11,
+        'sauce': 'tomato',
     },
     'pizza du chef': {
         'ingredients': {'mozzarella': 3, 'pepper': 2, 'onion': 1, 'ham': 4},
         'price': 12,
+        'sauce': 'cream',
     }
 }
