76323335d2c19142d9e4600ab6ec67dc93e3781e
vince callarec <callarecvincent@yahoo.fr>
2023-04-20 13:00:13 +0200

    add new pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'pizza du chef': {
+        'ingredients': {'mozzarella': 3, 'pepper': 2, 'onion': 1, 'ham': 4},
+        'price': 12,
     }
 }
